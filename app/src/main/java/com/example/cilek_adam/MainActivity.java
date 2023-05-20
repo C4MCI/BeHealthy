ff52f443ae3dd9e890c32e86310189946c2c06fd
MustafaSPN <mustafaspn@hotmail.com>
2023-05-20 16:30:40 +0300

    giriş sayfası

--- app/src/main/java/com/example/cilek_adam/MainActivity.java
+++ app/src/main/java/com/example/cilek_adam/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.cilek_adam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((Button)findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(i);
+            }
+        });
+        ((Button)findViewById(R.id.button2)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MainActivity3.class);
+                startActivity(i);
+            }
+        });
     }
 }

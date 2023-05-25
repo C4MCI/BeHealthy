8a49e50f182039fd790099fbeac627231b0f9a93
MustafaSPN <mustafaspn@hotmail.com>
2023-05-26 02:12:05 +0300

    ilk sayfaya tr ve eng

--- app/src/main/java/com/example/cilek_adam/MainActivity.java
+++ app/src/main/java/com/example/cilek_adam/MainActivity.java
@@ -6,21 +6,43 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-
+Switch switchB;
+    Button b2;
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ((Button)findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
+
+        switchB = findViewById(R.id.switchMain);
+
+         b1 =findViewById(R.id.button);
+
+         b2 = findViewById(R.id.button2);
+        switchB.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    b1.setText(R.string.main_logIn_E);
+                    b2.setText(R.string.main_register_E);
+                }else{
+                    b1.setText(R.string.main_logIn_T);
+                    b2.setText(R.string.main_register_T);
+                }
+            }
+        });
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, logIn.class);
                 startActivity(i);
             }
         });
-        ((Button)findViewById(R.id.button2)).setOnClickListener(new View.OnClickListener() {
+        b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, register.class);

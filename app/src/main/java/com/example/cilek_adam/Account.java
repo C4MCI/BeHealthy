19bf1be8c005a87e60f4eeae59e3db685dd261e6
MustafaSPN <mustafaspn@hotmail.com>
2023-05-27 18:06:36 +0300

    back button

--- app/src/main/java/com/example/cilek_adam/Account.java
+++ app/src/main/java/com/example/cilek_adam/Account.java
@@ -20,7 +20,6 @@ public class Account extends AppCompatActivity {
 UserInfo info;
 TextView accountTV,nameTV,emailTV,yearsTV,weightTV,heightTV,sexTV;
     TextView nameTV2,emailTV2,yearsTV2,weightTV2,heightTV2,sexTV2,BMITV2,BMITV3;
-Button updateB;
 Switch Aswitch;
 FirebaseAuth mAuth;
 FirebaseUser mUser;
@@ -41,7 +40,6 @@ FirebaseUser mUser;
         weightTV = findViewById(R.id.accountWeightTV);
         heightTV = findViewById(R.id.accountHeightTV);
         sexTV= findViewById(R.id.accountSexTV);
-        updateB = findViewById(R.id.accountButton);
         Aswitch = findViewById(R.id.accountSwitch);
 
         nameTV2 = findViewById(R.id.accountNameTV2);
@@ -52,7 +50,6 @@ FirebaseUser mUser;
         sexTV2= findViewById(R.id.accountSexTV2);
         BMITV2 = findViewById(R.id.accountBMITV2);
         BMITV3 = findViewById(R.id.accountBMITV3);
-        updateB.setText(R.string.update_updateInfo_T);
 
         nameTV2.setText(":  "+info.getName());
         emailTV2.setText(":  "+mUser.getEmail());
@@ -66,14 +63,7 @@ FirebaseUser mUser;
 
 
 
-        updateB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),UpdateInfo.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+
         Aswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @SuppressLint("SetTextI18n")
             @Override
@@ -86,7 +76,6 @@ FirebaseUser mUser;
                     weightTV.setText(R.string.reg2_weight_E );
                     heightTV.setText(R.string.reg2_height_E);
                     sexTV.setText(R.string.account_sex_E);
-                    updateB.setText(R.string.update_updateInfo_E);
                     sexTV2.setText(":  "+info.getSex());
                     BMITV3.setText("-->  "+info.getBmiRate());
 
@@ -98,7 +87,6 @@ FirebaseUser mUser;
                     weightTV.setText(R.string.reg2_weight_T);
                     heightTV.setText(R.string.reg2_height_T);
                     sexTV.setText(R.string.account_sex_T);
-                    updateB.setText(R.string.update_updateInfo_T);
                     if(info.getSex().equals("Men")){ sexTV2.setText(":  "+"Erkek");}
                     else {sexTV2.setText(":  "+"Kadın");}
                     BMITV3.setText("-->  "+info.getBmiRateTR());

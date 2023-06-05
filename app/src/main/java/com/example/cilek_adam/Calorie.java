a1da21567183fc111c712f56159ec76c0c6827f6
MustafaSPN <mustafaspn@hotmail.com>
2023-06-05 15:03:59 +0300

    dil ve guncelleme

--- app/src/main/java/com/example/cilek_adam/Calorie.java
+++ app/src/main/java/com/example/cilek_adam/Calorie.java
@@ -1,13 +1,11 @@
 package com.example.cilek_adam;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.Html;
-import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -17,8 +15,6 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -65,7 +61,7 @@ public class Calorie extends AppCompatActivity {
         morningText = findViewById(R.id.calorie_morningText);
         noonText = findViewById(R.id.calorie_noonText);
         eveningText = findViewById(R.id.calorie_eveningText);
-        calorieSwitch = findViewById(R.id.calorieSwitch);
+        calorieSwitch = findViewById(R.id.waterSwitch);
         saveButton = findViewById(R.id.calorie_saveButton);
 
         calorieSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
